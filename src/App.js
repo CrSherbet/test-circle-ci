3fd11eeacf4be7a53b7d13dea1a367fd1cdd6b11
crsherbet <warisaracr.y@gmail.com>
2018-12-14 12:02:58 +0700

    add google analytic tag

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
           {process.env.REACT_APP_TESTMESS}
           <br/><br/>
           {process.env.REACT_APP_TESTMES}
+          <img src='https://i0.wp.com/www.wwhf.org/wp-content/uploads/2014/04/bokeh-cover-bg.jpg?ssl=1' alt='pic' />
+          <button onClick={() => alert('kickkkk')} >Kick MEEEE!</button>
         </header>
       </div>
     );
