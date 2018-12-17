4f876f52e11b68b2f57ddf4df643eada83645c78
crsherbet <warisaracr.y@gmail.com>
2018-12-17 10:55:17 +0700

    add form

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ class App extends Component {
           <br/><br/>
           {process.env.REACT_APP_TESTMES}
           <img src='https://i0.wp.com/www.wwhf.org/wp-content/uploads/2014/04/bokeh-cover-bg.jpg?ssl=1' alt='pic' />
-          <button onClick={() => alert('kickkkk')} >Kick MEEEE!</button>
+          <button id='ddd' onClick={() => alert('kickkkk')} >Kick MEEEE!</button>
+          <button id='sdd' onClick={() => alert('22222')} >222222</button>
+          <form id='oooo'>
+            <input type='text' />
+          </form>
         </header>
       </div>
     );
